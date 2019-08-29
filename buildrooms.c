4373446146e65a6c11b3e6890ab3912c96b31b85
Karen <karenl.harper88@gmail.com>
2019-08-28 20:22:15 -0700

    Implement correct number of rooms

--- buildrooms.c
+++ buildrooms.c
@@ -3,17 +3,27 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <dict.h>
 #include <unistd.h>
 #include <sys/stat.h>
 #include <sys/types.h>
 
 int main() 
 {
+    srand(time(NULL))
     char pid_str[80];
 
     int pid = getpid();
     sprintf(pid_str, "rooms.%d", pid);
-    mkdir(pid_str, 0700); 
+    mkdir(pid_str, 0700); //Make room directory, with read, write, and execute power
+
+    char *rooms[7] = {"Kitten","Gargoyle","Lizard","Facebook","Computer","Witchy","Vacuum","Wonder","Liquor","Pride"}; 
     
+    fprintf(sizeof(rooms));
+    rooms = {}
+    for (int i = 0;i<sizeof(rooms);i++) {
+        
+    }  
+
     return 0;
 }
