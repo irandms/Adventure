fe50f1ee656f6fa57e95151069c377091a9e359b
Karen <karenl.harper88@gmail.com>
2019-08-31 21:57:52 -0700

    Starting randomization of map work

--- buildrooms.c
+++ buildrooms.c
@@ -1,26 +1,28 @@
 // Buildrooms.c 
 // Creates a file with descriptions of rooms, to be used by adventure.c
 
+#include <time.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <dict.h>
+#include <string.h>
+#include <search.h>
 #include <unistd.h>
 #include <sys/stat.h>
 #include <sys/types.h>
 
 int main() 
 {
-    srand(time(NULL))
+    srand(time(NULL));
     char pid_str[80];
 
     int pid = getpid();
     sprintf(pid_str, "rooms.%d", pid);
     mkdir(pid_str, 0700); //Make room directory, with read, write, and execute power
 
-    char *rooms[7] = {"Kitten","Gargoyle","Lizard","Facebook","Computer","Witchy","Vacuum","Wonder","Liquor","Pride"}; 
+    char *rooms[10] = {"Kitten","Gargoyle","Lizard","Facebook","Computer","Witchy","Vacuum","Wonder","Liquor","Pride"}; 
     
-    fprintf(sizeof(rooms));
-    rooms = {}
+    printf(str(sizeof(rooms)));
+    //rooms = {}
     for (int i = 0;i<sizeof(rooms);i++) {
         
     }  
