f436130ea13e27f5d809ea1ff34a027e04669e8a
Karen <karenl.harper88@gmail.com>
2019-09-15 21:26:20 -0700

    Implemented shuffle algorithm

--- buildrooms.c
+++ buildrooms.c
@@ -10,6 +10,21 @@
 #include <sys/stat.h>
 #include <sys/types.h>
 
+//Implementation of the Fisher-Yates shuffle algorithm, taken from https://benpfaff.org/writings/clc/shuffle.html
+void shuffle(char *array, size_t n)
+{
+    size_t i;
+    size_t length = sizeof(array) / sizeof(array[0]);
+
+    for (i = 0; i < length - 1; i++) {
+        size_t j = i + rand() / (RAND_MAX / (length - i) + 1);
+        char temp = array[j];
+        array[j] = array[i];
+        array[i] = temp;
+    }
+}
+
+
 int main() 
 {
     srand(time(NULL));
@@ -20,12 +35,14 @@ int main()
     mkdir(pid_str, 0700); //Make room directory, with read, write, and execute power
 
     char *rooms[10] = {"Kitten","Gargoyle","Lizard","Facebook","Computer","Witchy","Vacuum","Wonder","Liquor","Pride"}; 
+     
+    //for(int loop = 0; loop < 10; loop++)
+    //  printf("%s ", rooms[loop]);
     
-    printf(str(sizeof(rooms)));
-    //rooms = {}
-    for (int i = 0;i<sizeof(rooms);i++) {
-        
-    }  
+    shuffle(*rooms, 10);  
+
+    //for(int loop = 0; loop < 10; loop++)
+    //  printf("%s ", rooms[loop]);
 
     return 0;
 }
